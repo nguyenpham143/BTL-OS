6c774fc3306fc6bd2d3e9c50731d921ffb269239
Viet Khoa Nguyen <110188326+nvkhoa1207@users.noreply.github.com>
2026-05-30 19:28:43 +0700

    Implement scheduler

--- src/sched.c
+++ src/sched.c
@@ -22,14 +22,87 @@ static struct queue_t running_list;
 #ifdef MLQ_SCHED
 static struct queue_t mlq_ready_queue[MAX_PRIO];
 static int slot[MAX_PRIO];
+
+static int in_ready[MAX_PRIO];     
+static int in_active[MAX_PRIO];     
+static int active_prio[MAX_PRIO];   
+static int active_count;
+static int total_ready;
+
+static int normalize_prio(int prio)
+{
+	if (prio < 0)
+		return 0;
+	if (prio >= MAX_PRIO)
+		return MAX_PRIO - 1;
+	return prio;
+}
+
+static void remove_active_prio(int prio)
+{
+	int i;
+
+	if (!in_active[prio])
+		return;
+
+	for (i = 0; i < active_count; i++) {
+		if (active_prio[i] == prio)
+			break;
+	}
+
+	for (; i < active_count - 1; i++)
+		active_prio[i] = active_prio[i + 1];
+
+	active_count--;
+	in_active[prio] = 0;
+}
+
+static void insert_active_prio(int prio)
+{
+	int i;
+
+	if (in_active[prio])
+		return;
+
+	if (!in_ready[prio] || slot[prio] <= 0)
+		return;
+
+	for (i = active_count; i > 0 && active_prio[i - 1] > prio; i--)
+		active_prio[i] = active_prio[i - 1];
+
+	active_prio[i] = prio;
+	active_count++;
+	in_active[prio] = 1;
+}
+
+static void update_prio_state(int prio)
+{
+	in_ready[prio] = !empty(&mlq_ready_queue[prio]);
+
+	if (in_ready[prio] && slot[prio] > 0)
+		insert_active_prio(prio);
+	else
+		remove_active_prio(prio);
+}
+
+static void reset_mlq_round(void)
+{
+	int i;
+
+	active_count = 0;
+
+	for (i = 0; i < MAX_PRIO; i++) {
+		slot[i] = MAX_PRIO - i;
+		in_active[i] = 0;
+		update_prio_state(i);
+	}
+}
 #endif
 
 int queue_empty(void) {
 #ifdef MLQ_SCHED
-	unsigned long prio;
-	for (prio = 0; prio < MAX_PRIO; prio++)
-		if(!empty(&mlq_ready_queue[prio])) 
-			return -1;
+	if (total_ready > 0)
+		return -1;
 #endif
 	return (empty(&ready_queue) && empty(&run_queue));
 }
@@ -40,8 +113,14 @@ void init_scheduler(void) {
 
 	for (i = 0; i < MAX_PRIO; i ++) {
 		mlq_ready_queue[i].size = 0;
-		slot[i] = MAX_PRIO - i; 
+		slot[i] = MAX_PRIO - i;
+		in_ready[i] = 0;
+		in_active[i] = 0;
+		active_prio[i] = 0;
 	}
+
+active_count = 0;
+total_ready = 0;
 #endif
 	ready_queue.size = 0;
 	run_queue.size = 0;
@@ -58,41 +137,33 @@ void init_scheduler(void) {
  */
 struct pcb_t * get_mlq_proc(void) {
 	struct pcb_t * proc = NULL;
-
+	int prio;
 	pthread_mutex_lock(&queue_lock);
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 *      It worth to protect by a mechanism.
 	 * */
-	for (int prio = 0; prio < MAX_PRIO; prio++) {
-		if (slot[prio] > 0 && !empty(&mlq_ready_queue[prio])) {
-			proc = dequeue(&mlq_ready_queue[prio]);
+	if (total_ready > 0 && active_count == 0)
+		reset_mlq_round();
+
+	if (active_count > 0) {
+		prio = active_prio[0];
+
+		proc = dequeue(&mlq_ready_queue[prio]);
+
+		if (proc != NULL) {
+			total_ready--;
 			slot[prio]--;
-			break;
+			update_prio_state(prio);
+			enqueue(&running_list, proc);
 		}
 	}
-
-	if (proc == NULL) {
-		for (int i = 0; i < MAX_PRIO; i++)
-			slot[i] = MAX_PRIO - i;
-
-		for (int prio = 0; prio < MAX_PRIO; prio++) {
-			if (slot[prio] > 0 && !empty(&mlq_ready_queue[prio])) {
-				proc = dequeue(&mlq_ready_queue[prio]);
-				slot[prio]--;
-				break;
-			}
-		}
-	}
-
-
-	if (proc != NULL)
-		enqueue(&running_list, proc);
 
 	pthread_mutex_unlock(&queue_lock);
-	return proc;	
+	return proc;
 }
 
 void put_mlq_proc(struct pcb_t * proc) {
+	int prio;
 	proc->krnl->ready_queue = &ready_queue;
 	proc->krnl->mlq_ready_queue = mlq_ready_queue;
 	proc->krnl->running_list = &running_list;
@@ -101,13 +172,22 @@ void put_mlq_proc(struct pcb_t * proc) {
 	 *       It worth to protect by a mechanism.
 	 * 
 	 */
+	prio = normalize_prio(proc->prio);
+	proc->prio = prio;
+
 	pthread_mutex_lock(&queue_lock);
+
 	purgequeue(&running_list, proc);
-	enqueue(&mlq_ready_queue[proc->prio], proc);
+	enqueue(&mlq_ready_queue[prio], proc);
+
+	total_ready++;
+	update_prio_state(prio);
+
 	pthread_mutex_unlock(&queue_lock);
 }
 
 void add_mlq_proc(struct pcb_t * proc) {
+	int prio;
 	proc->krnl->ready_queue = &ready_queue;
 	proc->krnl->mlq_ready_queue = mlq_ready_queue;
 	proc->krnl->running_list = &running_list;
@@ -116,8 +196,16 @@ void add_mlq_proc(struct pcb_t * proc) {
 	 *       It worth to protect by a mechanism.
 	 * 
 	 */
+	prio = normalize_prio(proc->prio);
+	proc->prio = prio;
+
 	pthread_mutex_lock(&queue_lock);
-	enqueue(&mlq_ready_queue[proc->prio], proc);
+
+	enqueue(&mlq_ready_queue[prio], proc);
+
+	total_ready++;
+	update_prio_state(prio);
+
 	pthread_mutex_unlock(&queue_lock);	
 }
 
